238687c8707841e40e2d745aed29834fa70a4712
kollurikarthik <146532958+kollurikarthik@users.noreply.github.com>
2025-10-24 10:39:46 +0530

    fixed entry point

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-require("./db/connection");
+require("../backend/src/db/connection");
 const app = express();
 const port = process.env.PORT || 8000;
-const studentrouter = require("./routers/routing");
+const studentrouter = require("../backend/src/routers/routing");
 
 app.use(cors());
 app.use(express.json());
